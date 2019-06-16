26da14f0402eac65cf051b0af39828e75ece4ab9
santoshphilip <no-reply.santoshphilip@pyenergyplus.org>
2019-06-16 07:23:50 -0700

    added LICENSE to some files

--- eppy3000/epMunch.py
+++ eppy3000/epMunch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Santosh Philip
+# Copyright (c) 2018-2019 Santosh Philip
 # =======================================================================
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
--- tests/schemafortesting.py
+++ tests/schemafortesting.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2019 Santosh Philip
+# =======================================================================
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# =======================================================================
 """the schema used ffor testing is in this module
 It will be loaded only once, since that is how imports work"""
 
