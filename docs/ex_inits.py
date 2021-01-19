91b1d83c5ec6e47835d1d706a161127e8c914bc1
santoshphilip <no-reply.santoshphilip@pyenergyplus.org>
2021-01-18 21:52:26 -0800

    added the new files

---
+++ docs/ex_inits.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2020 Santosh Philip
+# =======================================================================
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# =======================================================================
+
+"""some init routines to do the documentation"""
+from IPython.display import Image
+from IPython.display import (
+    display_pretty,
+    display_html,
+    display_jpeg,
+    display_png,
+    display_json,
+    display_latex,
+    display_svg,
+)
+
+# idfeditor = Image(filename="./images/idf3.png")
+# material_lists = Image(filename="./images/material_lists.png")
+# html_snippet1 = Image(filename="./images/html1.png")
+# html_snippet2 = Image(filename="./images/html2.png")
+# plantloop1 = Image(filename="./images/hhh1.png")
+# plantloop2 = Image(filename="./images/hhh_new.png")
+epjhtml = Image(filename="./images/htmlviewer/epjhtml.png")
+epmunchhtml = Image(filename="./images/htmlviewer/epmunchhtml.png")
+epobjectshtml = Image(filename="./images/htmlviewer/epobjectshtml.png")
