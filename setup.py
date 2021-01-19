bfb06d519a10e4e1e6d30b4fea44aeb2f170ffa3
santoshphilip <no-reply.santoshphilip@pyenergyplus.org>
2021-01-18 22:07:45 -0800

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pyenergyplus/eppy3000",
-    version="0.1.4",
+    version="version='0.1.5'",
     zip_safe=False,
 )
