b519b9d65acdf8c43544e37b6fae34e6dcccbc9a
santoshphilip <no-reply.santoshphilip@pyenergyplus.org>
2021-01-18 23:17:52 -0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["munch", "eppy"]
+requirements = ["munch", "eppy", "json2html"]
 
 setup_requirements = [
     "pytest-runner",
