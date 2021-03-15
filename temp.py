1b578af340c6c9731de4cd22cf7b6b2239d72fe0
santoshphilip <no-reply.santoshphilip@pyenergyplus.org>
2021-03-14 23:02:12 -0700

    new file:   example.txt     modified:   runningnotes.txt     modified:   temp.py

--- temp.py
+++ temp.py
@@ -3,23 +3,30 @@ import eppy3000.installlocation as installlocation
 # from eppy3000 import idfjsonconverter, installlocation
 
 fname = "a.idf"
-fname = "/Applications/EnergyPlus-9-3-0/ExampleFiles/1ZoneDataCenterCRAC_wApproachTemp.idf"
-with open(fname, 'r') as idfhandle:
-    version = idfjsonconverter.getidfversion(idfhandle)
 
-print(version)
-schemapath = installlocation.schemapath(version)
-# schemapath =
-print(schemapath)
-# epjtxt = idfjsonconverter.idf2json(open(fname, 'r'), open(schemapath, 'r'))
-# print(epjtxt)
+flist = "example.txt"
+for line in open(flist, 'r'):
+    fname = line.strip()
+    # print(line.strip())
+
+    # fname = "/Applications/EnergyPlus-9-3-0/ExampleFiles/1ZoneDataCenterCRAC_wApproachTemp.idf"
+    with open(fname, 'r') as idfhandle:
+        version = idfjsonconverter.getidfversion(idfhandle)
+
+    print(version)
+    schemapath = installlocation.schemapath(version)
+    # schemapath =
+    print(schemapath)
+    print(fname)
+    # epjtxt = idfjsonconverter.idf2json(open(fname, 'r'), open(schemapath, 'r'))
+    # print(epjtxt)
 
 
 
-# fname = "./eppy3000/resources/snippets/V9_3/smallfile.idf"
-# schemapath = "./eppy3000/resources/schema/V9_3/Energy+.schema.epJSON"
-# schemapath = "/Applications/EnergyPlus-9-1-0/Energy+.schema.epJSON"
+    # fname = "./eppy3000/resources/snippets/V9_3/smallfile.idf"
+    # schemapath = "./eppy3000/resources/schema/V9_3/Energy+.schema.epJSON"
+    # schemapath = "/Applications/EnergyPlus-9-1-0/Energy+.schema.epJSON"
 
-idfhandle = open(fname, 'r')
-jsonstr = idfjsonconverter.idf2json(idfhandle, open(schemapath, 'r'))
-print(jsonstr)
+    idfhandle = open(fname, 'r')
+    jsonstr = idfjsonconverter.idf2json(idfhandle, open(schemapath, 'r'))
+    # print(jsonstr)
