5230f3d4735859f9a1f9693ddae08007d1c986c2
Santosh Philip <santosh@noemail.com>
2023-10-15 20:02:18 -0700

    added license to following:
        modified:   eppy3000/experimental/script_checktoml.py
        modified:   eppy3000/experimental/script_convertunits.py
        modified:   eppy3000/experimental/script_printunits.py

--- eppy3000/experimental/script_checktoml.py
+++ eppy3000/experimental/script_checktoml.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2023 Santosh Philip
+# =======================================================================
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# =======================================================================
 """compare write toml with a read toml"""
 # run this script from the root folder of eppy3000
 
