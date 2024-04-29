5723e9b332e823634f567dfb396133d98a513e6f
Santosh Philip <santosh@noemail.com>
2024-04-29 10:44:49 -0700

    cleanup after fixing issue #103

--- tests/test_epjviewer.py
+++ tests/test_epjviewer.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Santosh Philip
+# Copyright (c) 2021, 2024 Santosh Philip
 # =======================================================================
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
